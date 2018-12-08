f9846f5cb3df856a8864367e042583046f0e0b4e
leinov <1425795233@qq.com>
2018-12-09 00:26:34 +0800

    page modify

--- react-multi-page-app/src/component/nav.js
+++ react-multi-page-app/src/component/nav.js
@@ -9,8 +9,8 @@ export default class Nav extends Component {
 			<div className="menu columns">
 				<div className="column is-2 logo"><img  src={logo} /></div>
 				<div className="columns column nav is-8">
-					<div className="nav-item"><a href= "/">Home</a></div>
-					<div className="nav-item"><a href= "/todo">Todo</a></div>
+					<div className="nav-item"><a href= "">Home</a></div>
+					<div className="nav-item"><a href= "todo">Todo</a></div>
 					<div className="nav-item"><a href= "https://github.com/leinov/webpack-react-multi-page/">Github</a></div>
 				</div>
 				<div className="column is-2"></div>
